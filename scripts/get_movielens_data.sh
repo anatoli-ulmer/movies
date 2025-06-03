dc3db4909a89a2e27feb6bafa22dedf7ce945e09
Anatoli Ulmer <anatoli.ulmer@gmail.com>
2025-06-03 10:32:42 +0000

    remove data zip file after unzip

--- scripts/get_movielens_data.sh
+++ scripts/get_movielens_data.sh
@@ -8,17 +8,20 @@ SOURCE_URL="https://files.grouplens.org/datasets/movielens/"
 DATETIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 DATETIME_SAVE=${DATETIME:0:4}${DATETIME:5:2}${DATETIME:8:2}_${DATETIME:11:2}${DATETIME:14:2}
 
-filenames=("ml-latest-small.zip" "ml-latest.zip" "ml-20m.zip")
+datasets=("ml-latest-small" "ml-latest" "ml-20m")
 
-for fn in ${filenames[*]}; do
+for ds in ${datasets[*]}; do
     echo $DATETIME
-    filepath=$DATAPATH/$fn
-    if [ -f $filepath ]; then
-        echo "File $fn aleady exists. No download necessary."
+    ds_path=$DATAPATH/$ds
+    fn=$ds_path.zip
+    if [ -d $ds_path ]; then
+        echo "Directory $ds_path aleady exists. No download necessary."
     else
-        echo "Downloading $fn"
-        wget -P $DATAPATH $SOURCE_URL$fn
-        echo "Unzipping $fn"
-        unzip $filepath -d $DATAPATH
-    fi    
+        echo "Downloading $ds.zip ..."
+        wget -P $DATAPATH $SOURCE_URL$ds.zip
+        echo "Unzipping $fn ..."
+        unzip $fn -d $DATAPATH
+        echo "Removing $fn ..."
+        rm $fn
+    fi     
 done
